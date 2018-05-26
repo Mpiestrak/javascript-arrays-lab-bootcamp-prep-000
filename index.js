f9a062cf26d1e859a6ad69fdec08d89c55b36e6c
Matthew Piestrak <mpiestrak@gmail.com>
2018-05-26 17:55:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,3 +19,7 @@ function prependKitten(Arnold){
   var arr = ["Arnold", ...kittens]
   return arr
 }
+function removeLastKitten(){
+  var cats = kittens.slice(0, kittens.length - 1)
+  return cats
+}
