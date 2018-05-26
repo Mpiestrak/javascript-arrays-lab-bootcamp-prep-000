e79211e3c0d5b6ec649bc4a53d8e45b6032884db
Matthew Piestrak <mpiestrak@gmail.com>
2018-05-26 17:39:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const app = "I don't do much."
 function destructivelyAppendKitten (Ralph){
   kittens.push("Ralph")
 }
-function PrependKitten(Ralph){
+function destructivelyPrependKitten(Ralph){
   kittens.unshift("Ralph")
 }
